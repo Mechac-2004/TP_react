37671f33e13d42aed0b630d5d3f018af6b3966a6
ASSONGBA Méchac <mechacassongba68@gmail.com>
2024-11-30 16:30:15 +0100

    Début de réalisation de la navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,29 @@
 import React from "react";
+import './header.css'
 
 
 export default function Header(){
 
 
     return(
-        <div>
-        heloo</div>
+        <header>
+        <div className="menu">
+            <div className="logo"><img src="./logo.jpg" alt=" logo du site" /></div>
+            <div className="nav-bar">
+                <ul>
+                    <li><a href="">About Us</a></li>
+                    <li><a href="">Living Room</a></li>
+                    <li><a href="">Kitchen</a></li>
+                    <li><a href="">Outdoors</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+        <div className="text-center">
+            <h1>Coming Home Never <br/>Felt So Good!</h1>
+            <p><i>Quam a diamlorem explicabo quos fugit, ut aliquam modi.</i></p>
+            <button>FIND YOUR STYLE</button>
+        </div>
+    </header>
     )
 }
---
+++ src/pages/nav.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/header"
+
+
+function Nav(){
+
+
+
+    return(
+        <Header/>
+    )
+}
+export default Nav
